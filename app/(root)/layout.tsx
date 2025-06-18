a26cf8b152628605ede9a4d0457b438037886d06
swastideep maharana <swastideep67maharana@gmail.com>
2025-06-18 14:23:30 +0530

    remove the extra upload button

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Sidebar from "@/components/Sidebar";
-import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
@@ -18,7 +17,6 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
       <Sidebar {...currentUser} />
 
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation {...currentUser} />
         <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
       </section>
